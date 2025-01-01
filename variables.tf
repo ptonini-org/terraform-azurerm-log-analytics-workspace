0661d4e9cbc0ef6138820e758c40ad8e59a3887f
ptonini <pedro.tonini@gmail.com>
2024-12-31 21:16:03 -0300

    chore: first release

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "name" {}
+
+variable "rg" {
+  type = object({
+    name     = string
+    location = string
+  })
+}
+
+variable "retention_in_days" {
+  default = 30
+}
+
+variable "daily_quota_gb" {
+  default = 0.5
+}
+
+variable "sku" {
+  default = "PerGB2018"
+}
+
+variable "exports" {
+  type = map(object({
+    destination_id = string
+    table_names    = set(string)
+  }))
+  default = {}
+}
+
+variable "export_plan" {
+  default = "Basic"
+}
